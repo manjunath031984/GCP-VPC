3910164ffca7ec35dd90b833dcabeae376e93148
manjunath <vannalmanju21@gmail.com>
2026-07-18 18:41:14 +0530

    updated versions.tf

--- versions.tf
+++ versions.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = "~> 1.15.0"
 
+  backend "gcs" {
+    bucket = "gcp-dev-july-2026-terraform-state"
+    prefix = "GCP-VPC"
+  }
+
   required_providers {
     google = {
       source = "hashicorp/google"
